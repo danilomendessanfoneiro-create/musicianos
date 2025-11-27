0adb0806f55544cac78f02096b8365acc585d86e
danilomendessanfoneiro-create <danilomendessanfoneiro@gmail.com>
2025-11-27 13:42:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠ Sem path, sem loadEnv, sem process.env
-// Vercel injeta as variáveis automaticamente no build.
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': '/src'
     }
   },
   build: {
