332df2a1821821f502b711f8c3f4bcef6d97d40f
danilomendessanfoneiro-create <danilomendessanfoneiro@gmail.com>
2025-11-27 23:00:39 -0300

    Update PrivateRoute.tsx

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import { getAuth } from 'firebase/auth'
+import { Navigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "../firebase"
 
 export default function PrivateRoute({ children }: { children: JSX.Element }) {
-  const auth = getAuth()
-  const user = auth.currentUser
-  if (!user) {
-    return <Navigate to="/login" replace />
-  }
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState<any>(null)
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (u) => {
+      setUser(u)
+      setLoading(false)
+    })
+    return unsub
+  }, [])
+
+  if (loading) return <div>Carregando...</div>
+
+  if (!user) return <Navigate to="/login" replace />
+
   return children
 }
